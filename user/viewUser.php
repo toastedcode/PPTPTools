130f17918eee62f0cd3f8c91a9eccad23dea718b
toastedcode <jasontost@gmail.com>
2018-12-22 06:24:19 -0500

    Fixed error in saving new users.

--- user/viewUser.php
+++ user/viewUser.php
@@ -175,7 +175,7 @@ HEREDOC;
 
          <div class="form-item">
             <div class="form-label">Password</div>
-            <input id="user-password-input" type="password" class="form-input-medium" name="username" form="input-form" style="width:150px;" value="$userInfo->username" $disabled />
+            <input id="user-password-input" type="password" class="form-input-medium" name="password" form="input-form" style="width:150px;" value="$userInfo->password" $disabled />
          </div>
 
       </div>
